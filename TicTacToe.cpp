4ad07e251331fdcbecba7e935f8f131b7eb77955
Elaine Yang <299747@beaverton.k12.or.us>
2016-10-03 13:01:39 -0700

    unfinished

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -7,18 +7,20 @@
 #include <string.h>
 using namespace std;
 
-
+bool checkwin(char player, char(*gameboard)[3]);
+void printboard(char(*gameboard)[3]);
 int main(){
 	char board [3][3];
 	for (int i = 0; i < 3; i++){
 		for(int j = 0; j < 3; j++){
-			board[i][j] = '';
+			board[i][j] = ' ';
 	        }
 	}								        
 	char (*gameboard)[3] = board;
 	char playerturn = 'X';
 	
 	cout << "What is your move Player " << playerturn << endl;
+	printboard(gameboard);
 
 }
 
@@ -27,8 +29,53 @@ void printboard(char(*gameboard)[3]) {
 	for(int i = 0; i < 3; i++){
 		cout << i;
 		for(int j = 0; j < 3; j++){
-			cout << [i][j]; 
+			cout << gameboard[i][j]; 
 		}
 		cout << endl;
 	}
 }
+
+bool checkwin(char player, char(*gameboard)[3]) {
+	bool playerwin = false;
+	if(gameboard[0][0] == player &&
+		gameboard[1][0] == player &&
+		gameboard[2][0] == player) {
+		playerwin = true;
+	}
+	if(gameboard[0][1] == player &&
+                gameboard[1][1] == player &&
+                gameboard[2][1] == player) {
+		playerwin = true;
+	}
+	if(gameboard[0][2] == player &&
+                gameboard[1][2] == player &&
+                gameboard[2][2] == player) {
+                playerwin = true;
+        }
+	if(gameboard[0][0] == player &&
+                gameboard[0][1] == player &&
+                gameboard[0][2] == player) {
+                playerwin = true;
+        }
+	if(gameboard[1][0] == player &&
+                gameboard[1][1] == player &&
+                gameboard[1][2] == player) {
+                playerwin = true;
+        }
+	if(gameboard[2][0] == player &&
+                gameboard[2][1] == player &&
+                gameboard[2][2] == player) {
+                playerwin = true;
+        }
+	if(gameboard[0][0] == player &&
+                gameboard[1][1] == player &&
+                gameboard[2][2] == player) {
+                playerwin = true;
+        }
+        if(gameboard[0][2] == player &&
+                gameboard[1][1] == player &&
+                gameboard[2][0] == player) {
+                playerwin = true;
+        }
+}
+
